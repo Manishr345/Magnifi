37ed829a5c4c43b2ca12d92f1010388276983b23
Aryan <aryanbkarki30@gmail.com>
2025-08-16 14:50:55 +0530

    gitignore updated

--- home/home js/burger.js
+++
@@ -1,9 +0,0 @@
-const burg = document.querySelector('.burger');
-const categories = document.querySelector('.categories');
-const nav = document.querySelector('.navbardark');
-
-burg.addEventListener('click', () => {
-    console.log('button clicked')
-    categories.classList.toggle('hidden');
-    nav.classList.toggle('height');
-})
